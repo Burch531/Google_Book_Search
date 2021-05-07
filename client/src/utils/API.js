5b064312490265497523c77e8ee5053e308d0bd8
Burchell <jlkaline@me.com>
2021-05-07 15:02:13 -0400

    src folder

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,15 @@
 import axios from "axios";
+const BaseUrl = "https://www.googleapis.com/books/v1/volumes?q="
 
 export default {
   // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
+  searchBooks: (query) => axios.get(BaseUrl + query),
   // Gets the book with the given id
   getBook: function(id) {
     return axios.get("/api/books/" + id);
   },
   // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
+  deleteBook: (id) => axios.delete("api/books/" + id),
   // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
+  saveBook: (bookData) => axios.post("/api/books", bookData),
 };
