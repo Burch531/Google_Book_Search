4ce9dbc9de31e2015d58e6c6e4da759d10c29187
Burchell <jlkaline@me.com>
2021-05-07 15:50:36 -0400

    update bg image

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,9 +5,7 @@ export default {
   // Gets all books
   searchBooks: (query) => axios.get(BaseUrl + query),
   // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
+  getBooks: () => axios.get("/api/books"),
   // Deletes the book with the given id
   deleteBook: (id) => axios.delete("api/books/" + id),
   // Saves a book to the database
