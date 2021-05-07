d1da183f1482942d36a05a3988cecf6615b917df
Burchell <jlkaline@me.com>
2021-05-07 14:24:38 -0400

    components

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./style.css";
 
 function Jumbotron({ children }) {
   return (
     <div
       style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
-    >
+      className="jumbotron text-center">
+        <h1>Goodle Books Search</h1>
       {children}
     </div>
   );
